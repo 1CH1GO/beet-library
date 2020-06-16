fa947560e710b41ac1f6890c1c934f576441f47f
v-O_O-v <56096830+1CH1GO@users.noreply.github.com>
2020-06-16 20:47:48 +0530

    cleaned

--- bbst/pb_ds_tree.cpp
+++ bbst/pb_ds_tree.cpp
@@ -1,8 +1,3 @@
-#ifndef call_from_test
-#include<bits/stdc++.h>
-using namespace std;
-#endif
-//BEGIN CUT HERE
 #include<ext/pb_ds/assoc_container.hpp>
 #include<ext/pb_ds/tree_policy.hpp>
 #include<ext/pb_ds/tag_and_trait.hpp>
@@ -14,9 +9,4 @@ using gtree = tree<T, null_type, less<T>, rb_tree_tag,
 // find_by_order(int k):
 // return the iterator of k-th smallest element (0-indexed)
 // order_of_key(T key):  return the index of key in tree
-//END CUT HERE
-#ifndef call_from_test
-signed main(){
-  return 0;
-}
-#endif
+
