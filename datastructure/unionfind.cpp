df7fe1d02cab2eb20778f192b1a717fab60defd1
v-O_O-v <56096830+1CH1GO@users.noreply.github.com>
2020-06-16 21:05:28 +0530

    Update unionfind.cpp

--- datastructure/unionfind.cpp
+++ datastructure/unionfind.cpp
@@ -1,25 +1,21 @@
-#ifndef call_from_test
-#include<bits/stdc++.h>
-using namespace std;
-#endif
-//BEGIN CUT HERE
 struct UnionFind{
   int num;
   vector<int> rs,ps;
   UnionFind(){}
   UnionFind(int n):num(n),rs(n,1),ps(n,0){iota(ps.begin(),ps.end(),0);}
   int find(int x){
-    return (x==ps[x]?x:ps[x]=find(ps[x]));
+    return (x == ps[x]?x: ps[x] = find(ps[x]));
   }
   bool same(int x,int y){
-    return find(x)==find(y);
+    return find(x) == find(y);
   }
   void unite(int x,int y){
-    x=find(x);y=find(y);
-    if(x==y) return;
-    if(rs[x]<rs[y]) swap(x,y);
-    rs[x]+=rs[y];
-    ps[y]=x;
+    x = find(x);
+    y = find(y);
+    if(x ==y ) return;
+    if(rs[x] < rs[y]) swap(x, y);
+    rs[x] += rs[y];
+    ps[y] = x;
     num--;
   }
   int size(int x){
@@ -29,10 +25,4 @@ struct UnionFind{
     return num;
   }
 };
-//END CUT HERE
-#ifndef call_from_test
-//INSERT ABOVE HERE
-signed main(){
-  return 0;
-}
-#endif
+
