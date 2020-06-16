f0c6c263f2f7e6a0c79d20c9eb673a46bd64ea4b
v-O_O-v <56096830+1CH1GO@users.noreply.github.com>
2020-06-16 22:27:08 +0530

    Update unionfind.cpp

--- datastructure/unionfind.cpp
+++ datastructure/unionfind.cpp
@@ -1,28 +1,28 @@
 struct UnionFind{
-  int num;
-  vector<int> rs,ps;
-  UnionFind(){}
-  UnionFind(int n):num(n),rs(n,1),ps(n,0){iota(ps.begin(),ps.end(),0);}
-  int find(int x){
-    return (x == ps[x]?x: ps[x] = find(ps[x]));
-  }
-  bool same(int x,int y){
-    return find(x) == find(y);
-  }
-  void unite(int x,int y){
-    x = find(x);
-    y = find(y);
-    if(x ==y ) return;
-    if(rs[x] < rs[y]) swap(x, y);
-    rs[x] += rs[y];
-    ps[y] = x;
-    num--;
-  }
-  int size(int x){
-    return rs[find(x)];
-  }
-  int count() const{
-    return num;
-  }
+    int num;
+    vector<int> rs,ps;
+    UnionFind(){}
+    UnionFind(int n):num(n),rs(n,1),ps(n,0){iota(ps.begin(),ps.end(),0);}
+    int find(int x){
+        return (x == ps[x]?x: ps[x] = find(ps[x]));
+    }
+    bool same(int x,int y){
+        return find(x) == find(y);
+    }
+    void unite(int x,int y){
+        x = find(x);
+        y = find(y);
+        if(x ==y ) return;
+        if(rs[x] < rs[y]) swap(x, y);
+        rs[x] += rs[y];
+        ps[y] = x;
+        num--;
+    }
+    int size(int x){
+        return rs[find(x)];
+    }
+    int count() const{
+        return num;
+    }
 };
 
